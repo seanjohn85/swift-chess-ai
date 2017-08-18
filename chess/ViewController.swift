272032258b3efe01c3cd7bb3571fda188e3570fe
John <johnkenny1985@gamil.com>
2017-08-18 19:45:58 +0100

    ui set up

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    ///ui elemenets
+    @IBOutlet var panOutlet: UIPanGestureRecognizer!
 
+    @IBOutlet var checkedLab: UILabel!
+    @IBOutlet var turnLab: UILabel!
+    
+    var piceDragged: UIChessPiece!
+    var sourceOrgin: CGPoint!
+    var destOrigin: CGPoint!
+    static var spaceFromLeftEdge = 8
+    static var spaceFromTopEdge = 132
+    static var tileSize = 38
+    
+    var myGame : ChessGame!
+    var chessPieces : [UIChessPiece]! = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
